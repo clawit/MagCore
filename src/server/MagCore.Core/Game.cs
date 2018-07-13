e254e612018eb165737bf4ef8f5b0b317833932f
Kevin <kevinyeti@gmail.com>
2018-07-13 17:21:35 +0800

    1.0.6

--- src/server/MagCore.Core/Game.cs
+++ src/server/MagCore.Core/Game.cs
@@ -162,6 +162,13 @@ namespace MagCore.Core
                 //win
                 _state = GameState.Done;
             }
+
+            var ts = DateTime.Now - CreateTime;
+            if (ts.TotalHours >= 5)
+            {
+                _state = GameState.Done;
+            }
+
         }
 
         private Dictionary<int, Player> DefeatPlayers = new Dictionary<int, Player>();
