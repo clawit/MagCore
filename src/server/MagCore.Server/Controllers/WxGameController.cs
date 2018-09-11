9da9f5fbca1efde7b8fb8c7811f963bf47195f5c
KevinShen <kevinyeti@gmail.com>
2018-09-11 16:37:56 +0800

    add minigame code

---
+++ src/server/MagCore.Server/Controllers/WxGameController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using MagCore.Core;
+using MagCore.Server.WxApi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace MagCore.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class WxGameController : Controller
+    { 
+        // GET api/game/5b4512fb673f4a638fe2907b7483c0ab
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            try
+            {
+                var content = WxMiniGame.GetGameCode(id);
+                var result = content.ReadAsByteArrayAsync().Result;
+                return new FileContentResult(result, "image/jpeg");
+            } 
+            catch (Exception ex)
+            {
+                return new ContentResult() { StatusCode = (int)HttpStatusCode.BadRequest, Content = ex.Message };
+            }
+        }
+
+        // GET api/game/Y5b4512fb673f4/id
+        [HttpGet("{code}/id")]
+        public ContentResult GetId(string code)
+        {
+            try
+            {
+                string id = GuidCompacter.Uncompact(code);
+                return new ContentResult() { StatusCode = (int)HttpStatusCode.OK, Content = id };
+            }
+            catch(Exception ex)
+            {
+                return new ContentResult() { StatusCode = (int)HttpStatusCode.BadRequest, Content = ex.Message };
+            }
+        }
+    }
+}
---
+++ src/server/MagCore.Server/WxApi/WxBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using SystemCommonLibrary.Json;
+
+namespace MagCore.Server.WxApi
+{
+    public static class WxBase
+    {
+        public static string BaseUrl = "https://api.weixin.qq.com/";
+        private static string _tokenUrl = "cgi-bin/token?grant_type=client_credential&appid=wx31b5b47955b57ce1&secret=595469d8b13028e589dedf9eb32bc9bf";
+
+        private static string _accessToken = string.Empty;
+        private static DateTime _expireTime = DateTime.Now.AddHours(-2);
+
+        public static string GetAccessToken()
+        {
+            if (DateTime.Now >= _expireTime)
+            {
+                try
+                {
+                    var client = new HttpClient() { BaseAddress = new Uri(BaseUrl) };
+                    var response = client.GetAsync(_tokenUrl).Result;
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var result = response.Content.ReadAsStringAsync().Result;
+                        dynamic json = DynamicJson.Parse(result);
+                        _accessToken = json.access_token;
+                        _expireTime = DateTime.Now.AddSeconds(json.expires_in);
+
+                        return _accessToken;
+                    }
+                    else
+                        throw new Exception("微信服务器错误");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+            else
+                return _accessToken;
+
+                  
+        }
+    }
+}
